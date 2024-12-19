b4ae763962a4e4a43c0991108a1a5454a9a4442c
woojeong <77192050+wooj22@users.noreply.github.com>
2024-12-20 01:44:27 +0900

    Feat | 메인게임 시작 UI 루틴

--- Assets/02. Scripts/MainUIManager.cs
+++ Assets/02. Scripts/MainUIManager.cs
@@ -5,10 +5,72 @@ using UnityEngine.UI;
 
 public class MainUIManager : MonoBehaviour
 {
+    [SerializeField] GameObject startInfoUI;
+    [SerializeField] GameObject controlInfoUI;
+    [SerializeField] Text stageNameText;
+    [SerializeField] Text adviceLable;
     [SerializeField] List<Image> fadeImageList;
+
+    private bool isStoped;
+
     private void Start()
     {
+        StartCoroutine(StartInfoUI());
+    }
+
+    // 게임 정보 UI
+    public IEnumerator StartInfoUI()
+    {
         StartCoroutine(FadeIn());
+
+        // 스테이지 정보
+        // 게임매니저에서 스테이지 정보 받아오는거 추가하기
+        stageNameText.text = "Stage 1"; 
+        yield return new WaitForSeconds(5f);
+
+        // 게임설명
+        startInfoUI.SetActive(false);
+        controlInfoUI.SetActive(true);
+    }
+
+    // 게임 시작 UI
+    public void OkButton()
+    {
+        StartCoroutine(GameStartUI());
+    }
+
+    IEnumerator GameStartUI()
+    {
+        // 준비
+        controlInfoUI.SetActive(false);
+        adviceLable.gameObject.SetActive(true);
+        AdviceLable("Ready...");
+        yield return new WaitForSeconds(3f);
+
+        // 시작
+        AdviceLable("Start!");
+        yield return new WaitForSeconds(3f);
+        AdviceLable("");
+    }
+
+    // 보조라벨
+    public void AdviceLable(string text)
+    {
+        adviceLable.text = text;
+    }
+
+    // 게임 중지
+    public void StopButton()
+    {
+        if (!isStoped)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+        
     }
 
     // 페이드인

b7f765ab9ba2fe9d6a514a2c7638077ee9f3ff34
woojeong <77192050+wooj22@users.noreply.github.com>
2024-12-19 22:00:13 +0900

    Feat | 메인메뉴
     - 스테이지 선택 및 시작

--- Assets/02. Scripts/MenuUIManager.cs
+++ Assets/02. Scripts/MenuUIManager.cs
@@ -5,5 +5,53 @@ using UnityEngine.UI;
 
 public class MenuUIManager : MonoBehaviour
 {
-    
+    [SerializeField] GameObject gameStartUI;
+    [SerializeField] GameObject stageUI;
+    [SerializeField] GameObject stageStartUI;
+
+    [SerializeField] Text StageDescriptionText;
+    private int stageNum;
+
+    // 메인 시작 버튼
+    public void GameStartButton()
+    {
+        gameStartUI.SetActive(false);
+        stageUI.SetActive(true);
+    }
+
+    // 스테이지 선택 뒤로 가기 버튼
+    public void BackButton()
+    {
+        gameStartUI.SetActive(true);
+        stageUI.SetActive(false);
+    }
+
+    // 스테이지 선택 버튼
+    public void StageButton(int stageNum)
+    {
+        switch (stageNum)
+        {
+            case 1:
+                StageDescriptionText.text = "Stage 1 \n Fun Fun";
+                stageNum = 1;
+                break;
+            case 2:
+                StageDescriptionText.text = "Stage 2 \n dddddd";
+                stageNum = 2;
+                break;
+            case 3:
+                StageDescriptionText.text = "Stage 3 \n cccccccc";
+                stageNum = 3;
+                break;
+
+        }
+        stageStartUI.SetActive(true);
+    }
+
+    // 스테이지 시작 버튼
+    public void StageStartButton()
+    {
+        PlayerPrefs.SetInt("StageNum", stageNum); 
+        //int stageNum = PlayerPrefs.GetInt("StageNum", 1);
+    }
 }
